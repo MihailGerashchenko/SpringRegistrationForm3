86875316212e802e0a300d937d710112e698a709
Mihail Gerashchenko <karelare22@gmail.com>
2021-09-23 17:10:03 +0300

    Commit 23/09/2021 17.09

---
+++ registration_form/src/main/java/ua/testing/registration_form/dto/NoteDTO.java
@@ -0,0 +1,19 @@
+package ua.testing.registration_form.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class NoteDTO {
+
+    private String names;
+
+    private String login;
+
+
+
+}
